fd03277f04bb04e1aa8aeab7c788f5ded587c0d0
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-06-30 16:43:47 -0500

    Refactor TodoList class

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
+let todoListManager = {
+  defaultList: [] 
+};
+
 class TodoList {
-
   constructor(title, description, dueDate, priority, notes = "", checklist = "", listName = "default") {
-    this._title = title;
-    this._description = description;
-    this._dueDate = dueDate;
-    this._priority = priority;
-    this._notes = notes;
-    this._checklist = checklist;
-    this._listName = listName;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+    this.checklist = checklist;
+    this.listName = listName;
   };
-  
-  viewTodoList() {
-    this._listName = {
-      title: this._title,
-      description: this._description,
-      dueDate: this.dueDate,
-      priority: this._priority,
-      notes: this._notes,
-      checklist: this._checklist
-    }
-    return this._listName;
-  }
-
 } 
 
 
@@ -34,6 +24,13 @@ class TodoList {
 // checklist = [{task:'organize desk', completed: false}, {task:'dust tv', completed: false}, {task: 'pick up socks', completed: false}]
 // listName = 'daily task 1'
 
+// examples
+// let dailyTodo = new TodoList('clean room', 'pick up loose items around room', 'tomorrow', 'medium', '', [{task:'organize desk', completed: false}, {task:'dust tv', completed: false}, {task: 'pick up socks', completed: false}]);
+// todoListManager.defaultList.push(dailyTodo);
+// console.log(todoListManager);
 
-let dailyTodo = new TodoList('clean room', 'pick up loose items around room', 'tomorrow', 'medium', '', [{task:'organize desk', completed: false}, {task:'dust tv', completed: false}, {task: 'pick up socks', completed: false}]).viewTodoList();
-console.log(dailyTodo);
+// let redTodo = new TodoList('paint', 'paint house', 'today', 'high');
+// console.log(redTodo.title);
+// todoListManager.importantList = [];
+// todoListManager.importantList.push(redTodo);
+// console.log(todoListManager);
